828d575e4f40662c9b85c126181191aee858e7ff
Penges1324 <1324a@web.de>
2021-07-09 23:28:29 +0200

    s

--- libs/config.js
+++ libs/config.js
@@ -1,17 +1,17 @@
-/*var dbOptions = {
+var dbOptions = {
 	host     : 'localhost',
     port     : '3306',
 	user     : 'pengesst',
 	password : 'Pesa!739',
 	database : 'pengesst_'
-};*/
+};
 
-var dbOptions = {
+/*var dbOptions = {
 	host     : 'localhost',
     port     : '3306',
 	user     : 'root',
 	password : '',
 	database : 'pengesst_'
-};
+};*/
 
 module.exports = {dbOptions: dbOptions};
